e7321470f337b9a18005f0e24dc711deeb0189f5
yunusemregul <yns__gl@hotmail.com>
2019-09-22 00:35:25 +0300

    initial

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ -I./ -I./Headers -I./Shaders -O2 -lGL -lglfw -lGLEW -lglut -o bsp-viewer main.cpp
---
+++ shaders/simpleshader_fragments.glsl
@@ -0,0 +1,12 @@
+#version 330 core
+
+// Input data
+in vec4 vertexColor;
+
+// Output data
+out vec4 color;
+
+void main()
+{ 
+	color = vertexColor;
+}
